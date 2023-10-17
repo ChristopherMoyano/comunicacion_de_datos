fc0bc214cb5af3759c290eb278fea35ad802a6f6
Christopher Moyano <cmoyano@ing.ucsc.cl>
2023-10-17 16:12:05 -0300

    termino de la tarea

--- tarea2.py
+++ tarea2.py
@@ -1,3 +1,16 @@
+import os 
+import time
+nombres_carpetas = os.listdir('datos/') #busco los nombres de las carpetas donde tengo los archivos
+
+#defino una funcion para buscar los nombres de todos los archivos en cada carpeta 
+def buscar_archivo(ruta):
+    archivos_texto = []
+    archivos = os.listdir(ruta)
+    for archivo in archivos:
+        if archivo[-4:] == '.txt':
+            archivos_texto.append(archivo)
+    return archivos_texto
+
 #funcion xor
 def xor(a,b):
     if(a==b):
@@ -5,27 +18,36 @@ def xor(a,b):
     else:
         return 1
 
-c_aux=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-c=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-contador=1
-f = open ('dataset1/data_150.txt', 'r')
-data=f.read()
-for input in data:
-    input = int(input)
-    aux=xor(input,c_aux[15])
-    aux_1=xor(aux,c_aux[4])
-    aux_2=xor(aux,c_aux[11])
-    for a in range(16):
-        i=15-a
-        c_aux[i]=c_aux[i-1]
-    c_aux[0]=aux
-    c_aux[5]=aux_1
-    c_aux[12]=aux_2
-    for i in range(16):
-        c[i] = c_aux[15-i]
-    print("step " + str(contador))
-    print(c)
-    contador=contador+1
-f.close()
 
+#codigo
+#en cada carpeta busco los archivos de texto para realizar crc en cada uno de ellos
+for carpeta in nombres_carpetas:
+    ruta = 'datos/' +  carpeta
+    archivos_texto = buscar_archivo(ruta)
+    for texto in archivos_texto:
+        with open(ruta + '/' + texto, 'r' ) as f: #abro el archivo
+            print()
+            print()
+            print(texto) #imprimo el nombre de archivo en el que estoy realizando la accion
+            c_aux=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] #creacion de una variable auxiliar ya que los datos se imprimiran al reves y se guardaran en este
+            c=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] #creacion de variable c donde se guardaran los datos en orden correcto
+            contador=1 #contador para contar el numero de pasos que se realizan
+            data=f.read() #lectura del archivo de texto
+            for input in data:
+                input = int(input)
+                aux=xor(input,c_aux[15])
+                aux_1=xor(aux,c_aux[4])
+                aux_2=xor(aux,c_aux[11])
+                for a in range(16):
+                    i=15-a
+                    c_aux[i]=c_aux[i-1]
+                c_aux[0]=aux
+                c_aux[5]=aux_1
+                c_aux[12]=aux_2
+                for i in range(16):
+                    c[i] = c_aux[15-i]
+                print("step " + str(contador))
+                print(c)
+                contador=contador+1
+        os.system("pause")
 
